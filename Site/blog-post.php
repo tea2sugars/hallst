10a1e90eda283113c577ad6113f098d7a4253db5
Rik Barwick <rik@creativitea.co.uk>
2016-09-08 16:50:57 +0100

    Sync

--- Site/blog-post.php
+++ Site/blog-post.php
@@ -24,6 +24,18 @@
 	
 	<!-- Custom css -->
 	<link href="assets/css/custom.css" rel="stylesheet">
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-76522032-1', 'auto');
+	  ga('send', 'pageview');
+	
+	</script>
+	
 </head>
 <body>
 
@@ -211,7 +223,7 @@
 		<hr class="divider-w">
 		<!-- /DIVIDER -->
 
-	<?php include('assets/php/includes/footer.php');?>
+	<?php include('assets/php/includes/footer.html');?>
 
 	</div>
 	<!-- /WRAPPER -->
--- Site/index.php
+++ Site/index.php
@@ -28,9 +28,20 @@
 	<!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel='stylesheet' type='text/css'>
+    
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-76522032-1', 'auto');
+	  ga('send', 'pageview');
+	
+	</script>
     
 </head>
 <body>
@@ -52,15 +63,16 @@
 				<!-- SLIDE -->
 				<li class="bg-dark">
 
-					<div class="slidebg slide-zoom" style="background-image:url(assets/images/hadleigh-gant-room.jpg)"></div>
+					<div class="slidebg slide-zoom" style="background-image:url(assets/images/hall-street-header.jpg)"></div>
 
 					<div class="hs-caption container">
 						<div class="caption-content">
-							<div class="hs-title-size-3 font-alt m-b-20">
-								Hall St.
+							<div class="hs-title-size-3 font-alt m-b-10">
+								<img src="assets/images/hall-street-logo.png" alt="hall-street-logo">
 							</div>
-							<div class="hs-title-size-1">
-								<a href="#" class="btn btn-primary btn-round btn-lg">View more</a>
+							<div class="hs-title-size-1 font-alt">
+								<strong>At Long Melford &amp; Hadleigh</strong><br/>
+								<a href="#stores" class="btn btn-primary btn-round btn-lg m-t-20 section-scroll">Find Us</a>
 							</div>
 						</div>
 					</div>
@@ -71,15 +83,16 @@
 				<!-- SLIDE -->
 				<li class="bg-dark">
 
-					<div class="slidebg slide-zoom" style="background-image:url(assets/images/hall-street-header.jpg)"></div>
+					<div class="slidebg slide-zoom" style="background-image:url(assets/images/hadleigh-gant-room.jpg)"></div>
 
 					<div class="hs-caption container">
 						<div class="caption-content">
-							<div class="hs-title-size-3 font-alt m-b-20">
-								Men's Fashion Store.
+							<div class="hs-title-size-3 font-alt m-b-10">
+								Hadleigh Refit
 							</div>
-							<div class="hs-title-size-1">
-								<a href="#" class="btn btn-primary btn-round btn-lg">View more</a>
+							<div class="hs-title-size-1 font-alt">
+								<strong>We've made some changes to our Hadleigh store</strong><br/>
+								<!--<a href="#" class="btn btn-primary btn-round btn-lg m-t-20">View more</a>-->
 							</div>
 						</div>
 					</div>
@@ -94,11 +107,11 @@
 
 					<div class="hs-caption container">
 						<div class="caption-content">
-							<div class="hs-title-size-3 font-alt m-b-20">
+							<div class="hs-title-size-3 font-alt m-b-10">
 								Summer Collection
 							</div>
-							<div class="hs-title-size-1">
-								<a href="#" class="btn btn-primary btn-round btn-lg">View more</a>
+							<div class="hs-title-size-1 font-alt">
+								<strong>In store now</strong>
 							</div>
 						</div>
 					</div>
@@ -119,16 +132,31 @@
 
 		
 
-		<section class="module-small">
+		<section class="module-small" id="about">
 
 			<div class="container">
 
-				<div class="row">
+				<div class="row" >
 
 
-					<h2 class="module-title font-alt align-center">About Hall Street</h2>
-					<div class="col-sm-6 col-sm-offset-3">
-							<p class="align-center">The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators.</p>
+					<h2 class="font-alt align-center">Quality. Simple. Trust.</h2>
+					<div class="col-sm-12 col-md-10 col-md-offset-1">
+						<h4 class="align-center">These are 3 words that sum up “Hall Street” and the values we live by.</h4>
+							<div class="text-columns">
+								
+								<p>Hall Street is a traditional “bricks and mortar” retailer serving Suffolk men and women business casual and leisurewear, wholly owned by Ian Beddis and Sean Garnham.</p>
+								
+								<p>The highest quality is offered in all aspects from service to product and without compromise. We work to understand our clients and offer them a classic approach that's flexible and clear. This helps us build reliable foundations which our clients can depend on and trust.</p>
+							
+								<p> Our approach to customers is warm and friendly – we deliver on the values above and reward our customers with an experience that they can value, something of a scarcity in this digital age of retail. </p>
+								<p>Hall Street has been awarded Suffolk’s Heartland Partner for the Gant brand, it’s ‘go-to retailer’ - offering both menswear, women’s clothing and footwear. Each of our brand partners have been selected to align to the Hall Street customer with a reliable, classic quality to it's identity.</p>
+							</div>
+					</div>
+					
+					<div class="row">
+						<div class="align-center">
+							<a href="#stores" class="btn btn-primary btn-round btn-lg section-scroll">Find Us</a>
+						</div>
 					</div>
 
 				</div>
@@ -137,128 +165,306 @@
 
 		</section>
 		
+		<!-- DIVIDER -->
+		<hr class="divider-g">
+		<!-- /DIVIDER -->
 		
-		<section class="module-small">
+		<section class="module-small" id="brands">
 
 			<div class="container">
+					<h2 class="font-alt align-center">Our Brands</h2>
 
 				<div class="row multi-columns-row post-columns">
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/gant-listing.jpg" alt="Gant">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Gant</h2>
+							</div>
+							<div class="post-entry">
+								<p>Established in 1949, Gant is a lifestyle brand with an American Sportswear Heritage.</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/gant-women-listing-new.jpg" alt="Gant Women">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Gant Women</h2>
+							</div>
+							<div class="post-entry">
+								<p>Relaxed American style with a hint of European sophistication.</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/eden-park-listing.jpg" alt="Eden Park">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Eden Park</h2>
+							</div>
+							<div class="post-entry">
+								<p>Created by French rugby internationals, Eden Park is now a leading fashion brand.</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/timberland-listing.jpg" alt="Timberland">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Timberland</h2>
+							</div>
+							<div class="post-entry">
+								<p>Timberland is committed to "Doing Well and Doing Good" by delivering world-class products.</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/henri-lloyd-listing.jpg" alt="Henri Lloyd">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Henri Lloyd</h2>
+							</div>
+							<div class="post-entry">
+								<p>Henri Lloyd, the original British sailing brand was founded in Manchester, England by Henri Strzelecki MBE</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/sand-listing.jpg" alt="Sand">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Sand</h2>
+							</div>
+							<div class="post-entry">
+								<p>The design of the Danish Fashion house SAND Copenhagen is driven by passion, creativity and innovation.</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/Z.jpg" alt="Chatham">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Chatham</h2>
+							</div>
+							<div class="post-entry">
+								<p>A British brand trading on its nautical heritage</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/Z-1.jpg" alt="Crew Clothing">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Crew Clothing</h2>
+							</div>
+							<div class="post-entry">
+								<p>Established in 1949, Gant is a lifestyle brand with an American Sportswear Heritage.</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					<!-- POST ITEM -->
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/vouchers.jpg" alt="Gift Vouchers">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Gift Vouchers</h2>
+							</div>
+							<div class="post-entry">
+								<p>Personalised Gift Vouchers</p>
+							</div>
+					
+						</div>
+					</div>
+					<!-- /POST ITEM -->
+					
+								</div>
 
-					<!-- POST ITEM -->
-					<div class="col-sm-6 col-md-4 col-lg-4">
-						
-						<div class="post">
-							<div class="post-thumbnail">
-								<img src="assets/images/post-1.jpg" alt="">
-							</div>
-							<div class="post-header">
-								<h2 class="post-title font-alt">Our trip to the Alps</h2>
-							</div>
-							<div class="post-entry">
-								<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-							</div>
-					
-						</div>
-					</div>
-					<!-- /POST ITEM -->
-					<!-- POST ITEM -->
-					<div class="col-sm-6 col-md-4 col-lg-4">
-						
-						<div class="post">
-							<div class="post-thumbnail">
-								<img src="assets/images/post-1.jpg" alt="">
-							</div>
-							<div class="post-header">
-								<h2 class="post-title font-alt">Our trip to the Alps</h2>
-							</div>
-							<div class="post-entry">
-								<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-							</div>
-					
-						</div>
-					</div>
-					<!-- /POST ITEM -->
-					<!-- POST ITEM -->
-					<div class="col-sm-6 col-md-4 col-lg-4">
-						
-						<div class="post">
-							<div class="post-thumbnail">
-								<img src="assets/images/post-1.jpg" alt="">
-							</div>
-							<div class="post-header">
-								<h2 class="post-title font-alt">Our trip to the Alps</h2>
-							</div>
-							<div class="post-entry">
-								<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-							</div>
-					
-						</div>
-					</div>
-					<!-- /POST ITEM -->
-					<!-- POST ITEM -->
-					<div class="col-sm-6 col-md-4 col-lg-4">
-						
-						<div class="post">
-							<div class="post-thumbnail">
-								<img src="assets/images/post-1.jpg" alt="">
-							</div>
-							<div class="post-header">
-								<h2 class="post-title font-alt">Our trip to the Alps</h2>
-							</div>
-							<div class="post-entry">
-								<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-							</div>
-					
-						</div>
-					</div>
-					<!-- /POST ITEM -->
-					<!-- POST ITEM -->
-					<div class="col-sm-6 col-md-4 col-lg-4">
-						
-						<div class="post">
-							<div class="post-thumbnail">
-								<img src="assets/images/post-1.jpg" alt="">
-							</div>
-							<div class="post-header">
-								<h2 class="post-title font-alt">Our trip to the Alps</h2>
-							</div>
-							<div class="post-entry">
-								<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-							</div>
-					
-						</div>
-					</div>
-					<!-- /POST ITEM -->
-					<!-- POST ITEM -->
-					<div class="col-sm-6 col-md-4 col-lg-4">
-						
-						<div class="post">
-							<div class="post-thumbnail">
-								<img src="assets/images/post-1.jpg" alt="">
-							</div>
-							<div class="post-header">
-								<h2 class="post-title font-alt">Our trip to the Alps</h2>
-							</div>
-							<div class="post-entry">
-								<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-							</div>
-					
-						</div>
-					</div>
-					<!-- /POST ITEM -->
-	
+			</div>
+			
+		</section>
+		<!-- DIVIDER -->
+		<hr class="divider-g">
+		<!-- /DIVIDER -->
+		<section class="module-small" id="stores">
+			<div class="container">
 
+				<div class="row">
+			
+					<h2 class="font-alt align-center">Our Stores</h2>
+					
+							<div class="col-sm-12 col-md-10 col-md-offset-1">
+								<h4 class="align-center">The Home of the Suffolk Man &amp; Woman</h4>
+									<div class="text-columns-2">
+										
+										<p>Hall Street welcomes new customers from across East Anglia including Bury St. Edmunds, Ipswich, Woodbridge, Colchester, Sudbury, Southwold, Aldeburgh, Framlingham, Thetford, Diss, Norwich and Cambridge. </p>
+		
+										<p>Based in Long Melford and Hadleigh, both desired locations to live and visit. Each boasts strong tourist and consumer offerings of shopping, dining and exploring.</p>
+		
+										<p>Customer convenience is strategic to Hall Street – with free parking outside, perusing within a relaxed laid-back environment is stark contrast to nearby town centres.</p>
+									</div>
+							</div>
 				</div>
+			</div>
 
+			<div class="container" id="contact">
+				<div class="row">
+					<div class="col-sm-12 col-md-6">
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/longmelford.jpg" alt="Hall Street - Long Melford">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Long Melford</h2>
+							</div>
+							<div class="post-entry">
+								<div class="row">
+									<div itemscope itemtype="http://schema.org/LocalBusiness">
+										<div class="col-sm-12 col-md-6">
+										
+											   <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+												 <p>
+
+											     <span itemprop="streetAddress">Hall Street</span><br/>
+											     <span itemprop="addressLocality">Long Melford</span><br/>
+											     <span itemprop="addressRegion">Suffolk</span><br/>
+											     <span itemprop="postalCode">CO10 9JL</span>
+												</p>
+												<p><a href="https://www.google.com/maps/dir/Current+Location/Hall+St,+Long+Melford,+Sudbury,+Suffolk+CO10,+UK"><span class="icon-map-pin"></span> Get Directions</a></p>
+											   </div>											
+											
+										</div>
+										
+										<div class="col-sm-12 col-md-6">
+										   
+										   <p>
+											   <span itemprop="openingHours" content="Tu,We,Th,Fr,Sa 10:00-17:00">Opening times:<br/>Tuesday to Saturday 10am to 5pm</span><br/>
+											   <span itemprop="openingHours" content="Su 11:00-15:00">Sunday 11am to 3pm</span><br/>
+											   Monday Closed.
+											</p>
+											<p>Phone: <span itemprop="telephone">01787 464828</span></p>
+											
+										</div>
+										
+									</div>
+								</div>
+								</div>
+									
+											
+									
+							</div>
+					
+						</div>
+					<div class="col-sm-12 col-md-6">
+						<div class="post">
+							<div class="post-thumbnail">
+								<img src="assets/images/hadleigh.jpg" alt="Hall Street - Hadleigh">
+							</div>
+							<div class="post-header">
+								<h2 class="post-title font-alt">Hadleigh</h2>
+							</div>
+							<div class="post-entry">
+								<div class="row">
+									<div itemscope itemtype="http://schema.org/LocalBusiness">
+										<div class="col-sm-12 col-md-6">
+										
+											   <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+												 <p>
+
+											     <span itemprop="streetAddress">99 High Street</span><br/>
+											     <span itemprop="addressLocality">Hadleigh, Ipswich</span><br/>
+											     <span itemprop="addressRegion">Suffolk</span><br/>
+											     <span itemprop="postalCode">IP7 5EJ</span>
+												</p>
+												<p><a href="https://www.google.com/maps/dir/Current+Location/99+High+St,+Hadleigh,+Ipswich,+Suffolk+IP7+5EJ,+UK"><span class="icon-map-pin"></span> Get Directions</a></p>
+											   </div>	
+											   
+											
+										</div>
+										
+										<div class="col-sm-12 col-md-6">
+										   
+										   <p>
+											   <span itemprop="openingHours" content="Tu,We,Th,Fr,Sa 10:00-17:00">Opening times:<br/>Tuesday to Saturday 10am to 5pm</span><br/>
+											   Sunday - Monday Closed.
+											</p>
+											<p>Phone: <span itemprop="telephone">01473 822554</span></p>
+											
+										</div>
+										
+									</div>
+								</div>
+								</div>
+									
+											
+									
+							</div>
+					
+						</div>
+					</div>
+					
+				</div>
 			</div>
 			
 		</section>
 
 		<!-- DIVIDER -->
-		<hr class="divider-w">
+		<hr class="divider-g">
 		<!-- /DIVIDER -->
 
-		<?php include('assets/php/includes/footer.php');?>
+		<?php include('assets/php/includes/footer.html');?>
 
 	</div>
 	<!-- /WRAPPER -->
