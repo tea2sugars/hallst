f7284bd844f3762604f02293ec93d697798dfd38
Rik Barwick <rik@creativitea.co.uk>
2016-09-13 23:08:29 +0100

    perch on live server

--- Site/perch/config/config.php
+++ Site/perch/config/config.php
@@ -1,10 +1,10 @@
 <?php
     define('PERCH_LICENSE_KEY', 'P21509-XXW087-ULZ976-KBT373-JNT022');
 
-    define("PERCH_DB_USERNAME", 'root');
-    define("PERCH_DB_PASSWORD", 'root');
-    define("PERCH_DB_SERVER", "localhost");
-    define("PERCH_DB_DATABASE", "hallst");
+    define("PERCH_DB_USERNAME", 'hallstr_admin');
+    define("PERCH_DB_PASSWORD", 'TopGun');
+    define("PERCH_DB_SERVER", "213.143.20.228");
+    define("PERCH_DB_DATABASE", "hallstr_live");
     define("PERCH_DB_PREFIX", "perch2_");
     
     define('PERCH_TZ', 'UTC');
@@ -12,7 +12,7 @@
     define('PERCH_EMAIL_FROM', 'richard.barwick@googlemail.com');
     define('PERCH_EMAIL_FROM_NAME', 'Rik Barwick');
 
-    define('PERCH_LOGINPATH', '/hallst/Site/perch');
+    define('PERCH_LOGINPATH', '/perch');
     define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
     define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
 
